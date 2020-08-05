55e31a0c9946fe1e4c8af762be1eba3c342822af
Keycat <owo.nthu@outlook.com>
2020-08-05 15:50:06 +0800

    新增註解和補complent那行

--- code/11_pratice_read_test_seq.pl
+++ code/11_pratice_read_test_seq.pl
@@ -1,11 +1,11 @@
 open (FILE, "<three_seq.fa");# < 表示讀入
-$n = 0;
+$n = 0; # 用來顯示行數
 print "\n";
 $seq = "";
 while (defined ($line = <FILE> )){
     print ++$n.":";
 
-    if (substr($line,0,1) ne ">"){
+    if (substr($line,0,1) ne ">"){ #用substr來檢查是否為為不需要的列
         print $line;
         chomp $line;
         $seq .= $line;
@@ -22,5 +22,6 @@ print FILE2 $seq;
 close FILE2;
 
 open (FILE3, ">three_reverse.txt");
+$seq =~ tr/acgtACGT/tgcaTGCA/; #
 print FILE3 scalar reverse $seq;
 close FILE3;
